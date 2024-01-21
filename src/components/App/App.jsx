9e3bae99eafc5ed089dfee71819a35c039b6f382
bonny-art <svitlana.otenko@gmail.com>
2024-01-21 14:58:14 +0200

    refactors code using react hooks

---
+++ src/components/App/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+
+import {
+  Container,
+  Grid,
+  GridItem,
+  Header,
+  SearchForm,
+  Section,
+  Text,
+  Todo,
+} from 'components';
+
+import React from 'react';
+
+export const App = () => {
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('todos')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = text => {
+    const todo = {
+      id: nanoid(),
+      text,
+    };
+
+    setTodos(t => [...t, todo]);
+  };
+
+  const handleSubmit = data => {
+    addTodo(data);
+  };
+
+  const deleteTodo = id => {
+    setTodos(t => t.filter(item => item.id !== id));
+  };
+
+  return (
+    <>
+      <Header />
+      <Section>
+        <Container>
+          <SearchForm onSubmit={handleSubmit} />
+
+          {todos.length === 0 && (
+            <Text textAlign="center">There are no any todos ... </Text>
+          )}
+
+          <Grid>
+            {todos.length > 0 &&
+              todos.map((todo, index) => (
+                <GridItem key={todo.id}>
+                  <Todo
+                    id={todo.id}
+                    text={todo.text}
+                    counter={index + 1}
+                    onClick={deleteTodo}
+                  />
+                </GridItem>
+              ))}
+          </Grid>
+        </Container>
+      </Section>
+    </>
+  );
+};
